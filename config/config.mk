2c551b0d04adf7f5c32ef9d0ef44af3eb97694e6
ferrreo <harderthanfire@gmail.com>
2023-06-11 23:15:38 +0100

    Update config.mk

--- config/config.mk
+++ config/config.mk
@@ -16,7 +16,7 @@ DISTRO_PKGS=\
 	pika-baseos \
 	pika-amdgpu-core \
 	pika-amdgpu-switcher \
-	pika-session-hyprland \
+	pika-hyprland-session \
 	pika-hyprland-desktop-minimal \
 	dbus-x11 \
 	rsync \
