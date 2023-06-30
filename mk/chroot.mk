0e35aa02f9d59873c0b79d51543fd83304234fb2
ferrreo <harderthanfire@gmail.com>
2023-06-30 21:10:08 +0100

    Update chroot.mk

--- mk/chroot.mk
+++ mk/chroot.mk
@@ -193,6 +193,11 @@ $(BUILD)/pool: $(BUILD)/chroot
 		CLEAN=1 \
 		/iso/chroot.sh"
 
+	#Set up autologin
+	sudo $(CHROOT) "$@.partial" /bin/bash -e -c sed -e 's #[initial_session] [initial_session] g' -i /etc/greetd/config.toml
+	sudo $(CHROOT) "$@.partial" /bin/bash -e -c sed -e 's\#user = "pikaos"\user = "pikaos"\g' -i /etc/greetd/config.toml
+	sudo $(CHROOT) "$@.partial" /bin/bash -e -c sed -e 's\#command = "Hyprland"\command = "Hyprland"\g' -i /etc/greetd/config.toml
+
 	# Unmount chroot
 	"scripts/unmount.sh" "$@.partial"
 
