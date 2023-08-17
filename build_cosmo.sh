97fc8972d9b94fa05f0171568d07a0fe0bf6f65d
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-08-17 22:46:09 +0300

    add cosmo private build (abuse of power :P )

---
+++ build_cosmo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ARCH=amd64
+RELEASE=2304
+DESKTOP=Hyprland
+ISO_IMAGE=PikaOS-Cosmo-private-$DESKTOP-$RELEASE-$ARCH-$(date '+%y.%m.%d')
+
+gpg --keyserver keyserver.ubuntu.com --recv-keys 204DD8AEC33A7AFF
+
+apt install --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 \
+    sudo \
+    debootstrap \
+    germinate \
+    grub-efi-amd64-bin \
+    grub-pc-bin \
+    isolinux \
+    mtools \
+    ovmf \
+    qemu-efi \
+    qemu-kvm \
+    squashfs-tools \
+    erofs-utils \
+    xorriso \
+    zsync \
+    build-essential \
+    apt-utils \
+    dosfstools \
+    git \
+    coreutils
+    
+
+echo "Building: $ISO_IMAGE".iso
+cp ./Makefiles/cosmo ./Makefile
+make
+
+mkdir -p builds/
+mv build/pikaos/23.04/pikaos_23*.iso builds/"$ISO_IMAGE".iso || exit 7
+cd builds
+touch "$ISO_IMAGE".md5
+md5sum "$ISO_IMAGE".iso > "$ISO_IMAGE".md5
+
+rsync -azP ./ ferreo@direct.pika-os.com:/srv/www/pikaiso/
---
+++ config/config_cosmo.mk
@@ -0,0 +1,110 @@
+DISTRO_NAME=PikaOS
+
+DISTRO_VOLUME_LABEL=$(DISTRO_NAME) $(DISTRO_VERSION) amd64 Intel
+
+# Packages to install
+DISTRO_PKGS=\
+	ubuntu-minimal \
+	ubuntu-standard \
+	ubuntu-keyring \
+	pika-sources \
+	systemd \
+	zram-config \
+	fonts-ubuntu \
+	plymouth-theme-spinner \
+	plymouth-theme-pika-text \
+	pika-baseos \
+	pika-amdgpu-core \
+	pika-amdgpu-switcher \
+    greetd \
+    tuigreet\
+	pika-hyprland-session \
+	pika-hyprland-desktop-minimal \
+	dbus-x11 \
+    pika-hyprland-settings \
+	rsync \
+	apt-utils \
+	dosfstools \
+	intel-microcode \
+	amd64-microcode \
+	linux-firmware \
+	firmware-sof-signed \
+	iucode-tool \
+	setserial \
+	git \
+	wireguard-modules \
+	kernel-pika  \
+	open-vm-tools \
+	open-vm-tools-desktop \
+	f2fs-tools \
+	xfsprogs \
+        pika-hyprland-autologin \
+    refind
+	
+# Packages to install after (to avoid dependency issues)
+POST_DISTRO_PKGS=rsync \
+	build-essential \
+	pika-gameutils-meta \
+	pika-rocm-meta
+
+DISTRO_PARAMS+=amd_pstate=active
+
+# Packages to have in live instance
+LIVE_PKGS=\
+	casper \
+	expect \
+	gparted \
+	calamares-settings-pika-kde
+
+# Packages to remove from installed system (usually installed as Recommends)
+RM_PKGS=\
+	ibus-mozc \
+	imagemagick-6.q16 \
+	irqbalance \
+	mozc-utils-gui \
+	snapd \
+	unattended-upgrades \
+	xul-ext-ubufox \
+	ubuntu-session \
+	ubuntu-wallpapers \
+	tilix \
+	ibus \
+	gnome-shell \
+        nautilus \
+        gnome-sushi \
+        libmutter-12-0 \
+        mutter-common \
+        mutter-common-bin \
+        apport
+
+# Packages not installed, but that may need to be discovered by the installer
+MAIN_POOL=\
+	at \
+	dfu-programmer \
+	efibootmgr \
+	ethtool \
+	grub-efi-amd64 \
+	grub-efi-amd64-bin \
+	grub-efi-amd64-signed \
+	grub2-common \
+	grub-pc \
+	grub-pc-bin \
+	grub-gfxpayload-lists \
+	libfl2 \
+	libx86-1 \
+	lm-sensors \
+	plymouth-theme-spinner \
+	plymouth \
+	plymouth-label \
+	pm-utils \
+	postfix \
+	powermgmt-base \
+	python3-debian \
+	python3-distro \
+	python3-evdev \
+	python3-systemd \
+	vbetool \
+	xbacklight \
+	refind \
+	mokutil \
+	pika-refind-theme
